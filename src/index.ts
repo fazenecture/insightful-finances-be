88a662f4e21249a17aaa80e9ffb273f18092b097
fazenecture <vivekkumarsingh2002@gmail.com>
2026-01-06 03:16:21 +0530

    feat: base routes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 import morgan from "morgan";
+import { Request, Response } from "express";
 import express, { Application } from "express";
 import cors from "cors";
 import baseRouter from "./routes/index.router";
@@ -13,6 +14,22 @@ app.set("trust proxy", true);
 app.use(morgan("dev"));
 
 app.use("/api", baseRouter);
+
+
+app.get("/health", (req: Request, res: Response) => {
+  res.status(200).json({
+    uptime: process.uptime(),
+    hrtime: process.hrtime(),
+  });
+});
+
+app.use("*", (req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: "NOT_FOUND",
+  });
+});
+
 
 const PORT = process.env.PORT || 4000;
 
